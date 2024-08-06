ce9da873af250ce1680511f8437c570e26479c50
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-08-06 09:15:22 +0500

    feat(filter): implement character encoding

---
+++ src/main/java/com/will/filter/EncodingFilter.java
@@ -0,0 +1,17 @@
+package com.will.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(StandardCharsets.UTF_8);
+        servletResponse.setCharacterEncoding(StandardCharsets.UTF_8);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
