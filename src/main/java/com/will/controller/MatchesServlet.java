c00f480756c2bbd8505e78053ed7333d03c6bd61
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-08-07 11:02:27 +0500

    feat(servlet): get DTO values and send it to front end

---
+++ src/main/java/com/will/controller/MatchesServlet.java
@@ -0,0 +1,28 @@
+package com.will.controller;
+
+import com.will.dto.MatchDTO;
+import com.will.service.MatchService;
+import com.will.util.PathFinder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/matches")
+public class MatchesServlet extends HttpServlet {
+    private final MatchService service = new MatchService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pageStr = req.getParameter("page");
+        String filter = req.getParameter("filterByName");
+
+        List<MatchDTO> matches = service.findAll(pageStr, filter);
+        req.setAttribute("matches", matches);
+        req.getRequestDispatcher(PathFinder.find("matches")).forward(req, resp);
+    }
+}
---
+++ src/main/java/com/will/service/MatchService.java
@@ -0,0 +1,40 @@
+package com.will.service;
+
+import com.will.dto.MatchDTO;
+import com.will.model.Match;
+import com.will.repository.MatchRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchService {
+    private final int RECORDS_PER_PAGE = 10;
+    private final int DEFAULT_PAGE = 1;
+    private final MatchRepository matchRepository = new MatchRepository();
+
+    public List<MatchDTO> findAll(String pageStr, String filter) {
+
+        int page = DEFAULT_PAGE;
+        if (pageStr != null && pageStr.matches("^\\d+$")) {
+            page = Integer.parseInt(pageStr);
+        }
+        List<Match> matches = matchRepository.findAll(RECORDS_PER_PAGE, (page - 1) * RECORDS_PER_PAGE, filter);
+        return convertToMatchDTO(matches);
+    }
+
+    private List<MatchDTO> convertToMatchDTO(List<Match> matches) {
+        List<MatchDTO> matchDTOs = new ArrayList<>();
+        for (int i = 0; i < matches.size(); i++) {
+            Match match = matches.get(i);
+            matchDTOs.add(new MatchDTO(
+                    i + 1,
+                    match.getPlayer1().getName(),
+                    match.getPlayer2().getName(),
+                    match.getWinner().getName()
+            ));
+        }
+        return matchDTOs;
+    }
+
+
+}
